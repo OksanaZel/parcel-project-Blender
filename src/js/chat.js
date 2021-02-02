896476a22c6bf6eca9bf729c51c0dbe0da5f0101
Elena <e.mandrovska@gmail.com>
2021-02-03 00:06:51 +0200

    chat-button

---
+++ src/js/chat.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.chat-button').slick({
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        speed: 700,
+        easing: 'ease',
+        infinite: true,
+        waitForAnimate: false,
+        autoplay: true,
+        autoplaySpeed: 300,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+       
+    
+    });
+})
